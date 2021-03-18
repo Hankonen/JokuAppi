455025f01335b49d3044932f197702014cd62cd8
mysterio88 <t9suju00@students.oamk.fi>
2021-03-18 18:07:05 +0200

    Aloitus sivun tekemistä. TODO Spinner.

--- app/src/main/java/projekti/ampuappi/MainActivity.java
+++ app/src/main/java/projekti/ampuappi/MainActivity.java
@@ -3,6 +3,8 @@ package projekti.ampuappi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void onClickListener_StraightToLabor(View view)
+    {
+        Toast.makeText(getApplicationContext(),"Suoraan synnytykseen",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickListener_to_location(View view)
+    {
+        Toast.makeText(getApplicationContext(),"matkalla kohteesee",Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClickListener_how_to_act(View view)
+    {
+        Toast.makeText(getApplicationContext(),"Kuinka Toimia",Toast.LENGTH_SHORT).show();
+    }
+    public void onClickListener_onTheWay_or_in_location(View view)
+    {
+        Toast.makeText(getApplicationContext(),"matkalle vai kohteessa",Toast.LENGTH_SHORT).show();
     }
 }
