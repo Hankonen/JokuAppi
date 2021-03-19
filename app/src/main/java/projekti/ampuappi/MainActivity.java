57606ddabc299fe582134639281d7341e6f6d4dc
mysterio88 <t9suju00@students.oamk.fi>
2021-03-19 14:09:30 +0200

    Alert boxi etusivulle kuvan kanssa

--- app/src/main/java/projekti/ampuappi/MainActivity.java
+++ app/src/main/java/projekti/ampuappi/MainActivity.java
@@ -1,11 +1,19 @@
 package projekti.ampuappi;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
@@ -44,8 +52,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickListener_StraightToLabor(View view)
     {
-        Toast.makeText(getApplicationContext(),"Suoraan synnytykseen",Toast.LENGTH_SHORT).show();
-    }
+      //  Toast.makeText(getApplicationContext(),"Suoraan synnytykseen",Toast.LENGTH_SHORT).show();
+
+            final AlertDialog.Builder renameDialog = new AlertDialog.Builder(MainActivity.this);
+            renameDialog.setTitle("Suoraan Synnytykseen");
+            Context context = renameDialog.getContext();
+            final LinearLayout layout = new LinearLayout(context);
+            layout.setOrientation(LinearLayout.VERTICAL);
+            final TextView kohteessa = new EditText(context);
+            kohteessa.setText("Hoidetaan kohteessa jos:\n •\tSupistuksia on 1-2 minuutin välein säännöllisesti\n •\tSynnyttäjällä on ponnistamisen pakko\n •\tSikiön pää näkyy");
+            layout.addView(kohteessa);
+            renameDialog.setView(layout);
+            final Button Straight_to_labor = new Button(context);
+            final ImageView kuva = new ImageView(context);
+            kuva.setImageResource(R.drawable.ohje);
+            layout.addView(kuva);
+
+            renameDialog.setNegativeButton("Cancel", (dialog, which) -> dialog.cancel());
+
+            renameDialog.show();
+        };
+
 
     public void onClickListener_to_location(View view)
     {
