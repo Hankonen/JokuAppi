b4b50f9bfff5961b363a5f40fe960ee4d595412f
Keijo <t9tike00@students.oamk.fi>
2021-03-23 18:55:24 +0200

    Sivu indikaattori lisätty, buttonit liikkumiseen kesken.

--- app/src/main/java/projekti/ampuappi/SliderActivity.java
+++ app/src/main/java/projekti/ampuappi/SliderActivity.java
@@ -4,15 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SliderActivity extends AppCompatActivity {
 
     private ViewPager slideViewPager;
     private LinearLayout linearLayout;
 
+    private TextView[] pageIndikaattori;
     private SliderAdapter sliderAdapter;
+
+    private Button leftButton;
+    private Button rightButton;
+    private int currentPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +33,79 @@ public class SliderActivity extends AppCompatActivity {
         sliderAdapter = new SliderAdapter(this);
 
         slideViewPager.setAdapter(sliderAdapter);
+        addDotsIndikaattori(0);
+
+        slideViewPager.addOnPageChangeListener(viewListener);
+
+
+
     }
+
+    public void addDotsIndikaattori(int pPosition)
+    {
+        pageIndikaattori = new TextView[5];
+        linearLayout.removeAllViews();
+
+        for (int i = 0; i < pageIndikaattori.length; i++)
+        {
+            pageIndikaattori[i] = new TextView(this);
+            pageIndikaattori[i].setText(Html.fromHtml("&#8226;"));  //tekee ympyrät
+            pageIndikaattori[i].setTextSize(35);
+            pageIndikaattori[i].setTextColor(getResources().getColor(R.color.white_dots_color));
+
+            linearLayout.addView(pageIndikaattori[i]);
+
+
+
+        }
+
+        if (pageIndikaattori.length > 0)
+        {
+            pageIndikaattori[pPosition].setTextColor(getResources().getColor(R.color.black));
+        }
+    }
+
+    // tarkistetaan millä sivulla ollaan
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {  // positio == sivu
+
+            addDotsIndikaattori(position);
+            currentPage = position;
+            /*
+            if (position == 0)
+            {
+                rightButton.setEnabled(true);
+                leftButton.setEnabled(false);
+                //rightButton.setEnabled(true);
+                leftButton.setVisibility(View.INVISIBLE);
+            }
+
+            else if (position == pageIndikaattori.length -1)
+            {
+                rightButton.setEnabled(true);
+                leftButton.setEnabled(true);
+                leftButton.setVisibility(View.VISIBLE);
+                rightButton.setVisibility(View.VISIBLE);
+            }
+            else
+            {
+                rightButton.setEnabled(true);
+                leftButton.setEnabled(true);
+                rightButton.setVisibility(View.VISIBLE);
+                leftButton.setVisibility(View.VISIBLE);
+            }*/
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 }
