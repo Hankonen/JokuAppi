fe10c74a58e92d42d2279a5b0815244d0356a494
zapse <santeri.sako@gmail.com>
2021-03-24 16:12:28 +0200

    korttipaivitys

--- maxlengt.js
+++ maxlengt.js
@@ -4,7 +4,7 @@ function myFunction(){
     document.getElementById("demo").innerHTML = obj.name;
 */
     var mydata = JSON.parse(data);
-    //document.getElementById("demo").innerHTML = (mydata[0].name);
-    alert(mydata[0].name);
+    document.getElementById("demo").innerHTML = (mydata[0].name);
+    //alert(mydata[0].name);
     //document.getElementById("demo").innerHTML = mydata.name;
 }
