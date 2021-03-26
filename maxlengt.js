1eec314f57010294cdd6ab5f1a7cd508449bb557
zapse <santeri.sako@gmail.com>
2021-03-26 12:14:33 +0200

    lisatty vasta locaali paivitys metodi

--- maxlengt.js
+++ maxlengt.js
@@ -5,6 +5,24 @@ function myFunction(){
 */
     var mydata = JSON.parse(data);
     document.getElementById("demo").innerHTML = (mydata[0].name);
+    document.getElementById("demot").innerHTML = (mydata[0].nametwo);
+    
     //alert(mydata[0].name);
     //document.getElementById("demo").innerHTML = mydata.name;
 }
+
+function saveEdits(){
+    var editElem = document.getElementById("edit");
+    var userVersion = editElem.innerHTML;
+    localStorage.userEdits = userVersion;
+    document.getElementById("update").innerHTML="Edits saved!";
+    
+}
+
+function checkEdits() {
+
+    //find out if the user has previously saved edits
+    if(localStorage.userEdits!=null)
+    document.getElementById("edit").innerHTML = localStorage.userEdits;
+    
+    }
